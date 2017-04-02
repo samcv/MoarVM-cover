0f9186704af9eb7902c91d6d720cef683dabbfcc
Samantha McVey <samantham@posteo.net>
2017-04-02 15:15:02 -0700

    Rework merge-profraw.sh with new features and better error handling
    If given a command line argument, uses this instead of the default
    directory name of ./coverage
    In addition it has error handling in case the folder does not exist and
    also if the llvm-profdata has errors.

--- merge-profraw.sh
+++ merge-profraw.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env sh
 # Merges all the files created by nqp-profile
-COVERAGE_DIR="./coverage"
+if [ "$1" ]; then
+    COVERAGE_DIR="$1"
+    printf "Merging into folder '%s' via command line argument\n" "$COVERAGE_DIR"
+else
+    COVERAGE_DIR="./coverage"
+    printf "Merging into folder '%s' via default\n" "$COVERAGE_DIR"
+fi
 OUT_FILE="$COVERAGE_DIR/nqptestcov.profdata"
-llvm-profdata merge -o "$OUT_FILE" "$COVERAGE_DIR"/* && printf "Finished writing to $OUT_FILE\n"
+if [ -d "$COVERAGE_DIR" ]; then
+    FILES="$(find $COVERAGE_DIR -name '*.profraw')"
+    llvm-profdata merge -o "$OUT_FILE" $FILES
+    case "$?" in
+        0) printf "Finished writing to $OUT_FILE\n"
+        ;;
+        *) printf "Encountered errors. Make sure none of the paths have spaces in the names\n"
+        ;;
+    esac
+else
+    printf "Could not find folder '%s'\n" "$COVERAGE_DIR"
+fi
