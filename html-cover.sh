1dc58ec3a2e8409764bf8a127723cefccbf4496d
Samantha McVey <samantham@posteo.net>
2017-04-02 02:09:48 -0700

    Start of new much better script, instructions not updated yet
    Can see the results of this script at https://cry.nu/coverage

---
+++ html-cover.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env sh
+funct () {
+NQP_FOLDER="../nqp"
+./Configure.pl --compiler=clang --coverage --optimize=0 --debug=3 --prefix="$HOME/perl6" || exit
+make install || exit
+cd "$NQP_FOLDER" || exit
+./Configure.pl --prefix="$HOME/perl6" --backend=moar || exit
+make || exit
+if [ ! -f nqp-profile ]; then echo "can't find nqp-profile"; exit 1; fi;
+if [ -e coverage ]; then mv -v coverage coverage-$RANDOM || exit 1; fi
+# RUNNING TESTS HERE
+DATE_VERSION_HEADER="$(printf "%s\n%s" "$(date --utc)" "$(./nqp --version 2> /dev/null)" )"
+printf "Starting tests\n%s\n" "$DATE_VERSION_HEADER"
+mkdir -p html || exit
+PROVE_CMD="$(make -n test | tail -n 1 | sed 's/nqp-m/nqp-profile/')"
+eval "$PROVE_CMD"
+UNCLEAN_TEST=$?
+if [ ! -f merge-profraw.sh ]; then echo "Can't find merge-profraw.sh"; exit 1; fi
+./merge-profraw.sh
+# Fake tty so llvm-cov will output ANSI-color through a pipe
+faketty () { script -qfc "$(printf "%q " "$@")"; }
+# Generates ISO date format
+isodate () { date -u +"%Y-%m-%dT%H:%M:%SZ"; }
+# Filelist that is used for the coverage (variable not yet integrated)
+filelist () { find src -name '*.c' | grep -v unicode.c; }
+
+cd ../MoarVM || exit
+}
+if [ -e "html" ]; then mv -v html html-$RANDOM; fi
+llvm-cov show -format=html -instr-profile ../nqp/coverage/nqptestcov.profdata ./moar $(find src -name '*.c' | grep -v unicode.c) -output-dir=html
+llvm-cov show -format=html -instr-profile ../nqp/coverage/nqptestcov.profdata ./libmoar.* $(find src -name '*.c' | grep -v unicode.c) -output-dir=html
+
+mv html/index.html html/cov-index.html
+printf "_____________________________\nThere will be many errors above this line, and they are fine as long as only a couple below this line\n----------------------\n"
+faketty llvm-cov report -instr-profile ../nqp/coverage/nqptestcov.profdata ./moar $(find src -name '*.c' | grep -v unicode.c) | ansi2html > html/report-moar.html
+printf "There will be lots of errors below this line\n--------------------------------------\n"
+faketty llvm-cov report -instr-profile ../nqp/coverage/nqptestcov.profdata ./libmoar.* $(find src -name '*.c' | grep -v unicode.c) | ansi2html > html/report-libmoar.html
+if [ $UNCLEAN_TEST -gt 0 ]; then
+    UNCLEAN_TEST_TXT="Unclean test, returned status $UNCLEAN_TEST"
+fi
+if [ "$(which ansi2html)" ]; then
+    printf "%s\n%s\n%s\n" "$DATE_VERSION_HEADER" "$UNCLEAN_TEST_TXT" "$(faketty llvm-cov report -instr-profile ../nqp/coverage/nqptestcov.profdata ./libmoar.* $(find src -name '*.c' | grep -v unicode.c) )" | ansi2html > html/cov-index.html
+else
+    printf "Could not find ansi2html, so not able to output cov-index.html\n"
+fi
+exit $UNCLEAN_TEST
+# scp -r ./html/* root@cry.nu:/srv/cry.nu/coverage/
