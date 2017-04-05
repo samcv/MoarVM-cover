657c782d923616b1796e936a237b32b850895278
Samantha McVey <samantham@posteo.net>
2017-04-04 20:22:08 -0700

    Refactor html-cover.sh to use llvm 5 and more work on roast support

--- html-cover.sh
+++ html-cover.sh
@@ -45,34 +45,34 @@ stage_2 () {
 stage_3 () {
     check_prefix
     if [ ! "$DATE_VERSION_HEADER" ]; then DATE_VERSION_HEADER="$(printf "%s\n%s" "$(date --utc)" "$(cd ../nqp; ./nqp --version 2> /dev/null)" )"; fi
-
+    #PROFDATA='../rakudo/coverage/t/nqptestcov.profdata'
+    PROFDATA='../nqp/coverage/nqptestcov.profdata'
     # Filelist that is used for the coverage
     # main.c is in the `moar` binary. unicode.c is tens of thousands of lines long and
     # because of this is not included right now
-    filelist_libmoar () { llvm-cov report -instr-profile ../rakudo/coverage/t/nqptestcov.profdata ./libmoar.* | tail -n +3 | head -n -3 | cut -d ' ' -f 1 | grep -v 3rdparty; }
+    filelist_libmoar () { llvm-cov report -instr-profile $PROFDATA ./libmoar.* | tail -n +3 | head -n -3 | cut -d ' ' -f 1 | grep -v 3rdparty; }
     # We will generate the output of all the moar binary files just in case they change from main.c
     # or if under other distros there's extra files included
-    filelist_moar_bin () { llvm-cov report -instr-profile ../rakudo/coverage/t/nqptestcov.profdata ./moar | tail -n +3 | head -n -3 | cut -d ' ' -f 1 | grep -v 3rdparty; }
-    if [ ! -f "../nqp/coverage/nqptestcov.profdata" ]; then printf "Could not find ../nqp/coverage/nqptestcov.profdata\n"; exit 1; fi
+    filelist_moar_bin () { llvm-cov report -instr-profile $PROFDATA ./moar | tail -n +3 | head -n -3 | cut -d ' ' -f 1 | grep -v 3rdparty; }
+    if [ ! -f "$PROFDATA" ]; then printf "Could not find $PROFDATA\n"; exit 1; fi
     if [ -e "html" ]; then mv -v html "html-$RANDOM"; fi
-    llvm-cov show -format=html -instr-profile ../nqp/coverage/nqptestcov.profdata ./moar $(filelist_moar) -output-dir=html
-    printf "_____________________________\nThere will be many errors above this line, and they are fine as long as there are about 10 or less below this line\n----------------------\n"
-    llvm-cov show -format=html -instr-profile ../nqp/coverage/nqptestcov.profdata ./libmoar.* $(filelist_libmoar) -output-dir=html
-    mv html/index.html html/cov-index.html
+    llvm-cov show -format=html -instr-profile $PROFDATA ./moar $(filelist_moar_bin) -output-dir=html/moar
+    llvm-cov show -format=html -instr-profile $PROFDATA ./libmoar.* $(filelist_libmoar) -output-dir=html/libmoar
+    #mv html/index.html html/cov-index.html
     # If env var isn't set then we must be running stage_3 by itself, so set it to 0
     # to not cause an error when we check it numerically
     if [ "$UNCLEAN_TEST" = "" ]; then UNCLEAN_TEST=0; fi
     if [ "$UNCLEAN_TEST" -gt 0 ]; then
         UNCLEAN_TEST_TXT="Unclean test, returned status $UNCLEAN_TEST"
     fi
-    if [ "$(which ansi2html)" ]; then
-        printf "$DATE_VERSION_HEADER\n$UNCLEAN_TEST_TXT\n\n%s\n" "$(faketty llvm-cov report -instr-profile ../nqp/coverage/nqptestcov.profdata ./moar $(filelist_moar_bin) )" | ansi2html > html/report-moar.html && \
-            printf "Done creating html/report-moar.html\n"
-        printf "$DATE_VERSION_HEADER\n$UNCLEAN_TEST_TXT\n\n%s\n" "$(faketty llvm-cov report -instr-profile ../nqp/coverage/nqptestcov.profdata ./libmoar.* $(filelist_libmoar) )" | ansi2html > html/report-libmoar.html && \
-            printf "Done creating html/report-libmoar.html\n"
-    else
-        printf "Could not find ansi2html, so not able to output cov-index.html\n"
-    fi
+    #if [ "$(which ansi2html)" ]; then
+    #    printf "$DATE_VERSION_HEADER\n$UNCLEAN_TEST_TXT\n\n%s\n" "$(faketty llvm-cov report -instr-profile $PROFDATA ./moar $(filelist_moar_bin) )" | ansi2html > html/report-moar.html && \
+    #        printf "Done creating html/report-moar.html\n"
+    #    printf "$DATE_VERSION_HEADER\n$UNCLEAN_TEST_TXT\n\n%s\n" "$(faketty llvm-cov report -instr-profile $PROFDATA ./libmoar.* $(filelist_libmoar) )" | ansi2html > html/report-libmoar.html && \
+    #        printf "Done creating html/report-libmoar.html\n"
+    #else
+    #    printf "Could not find ansi2html, so not able to output function based reporting\n"
+    #fi
 
     exit $UNCLEAN_TEST
 }
