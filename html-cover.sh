61fbb724df8e9956a9241aecc3412744c8b455e6
Samantha McVey <samantham@posteo.net>
2017-04-13 01:55:46 -0700

    Some changes to be more portable to travis ci

--- html-cover.sh
+++ html-cover.sh
@@ -3,20 +3,26 @@
 faketty () { script -qfc "$(printf "%q " "$@")"; }
 # Generates ISO date format
 isodate () { date -u +"%Y-%m-%dT%H:%M:%SZ"; }
-
+HTML="html-cov"
 check_prefix () {
     if [ ! -f src/moar.c ]; then echo "You don't seem to be in the MoarVM folder. Can't see ./src/moar.c"; exit 1; fi
     if [ "$MOAR_PREFIX" = "" ]; then
         MOAR_PREFIX="$(pwd)/moar-cover" || exit 1
-        #export MOAR_PREFIX
         printf "MOAR_PREFIX env var not set, using '%s'\n" "$MOAR_PREFIX"
         export MOAR_PREFIX
     fi
+    if [ "$MOAR_FOLDER" = "" ]; then
+        MOAR_FOLDER="$(readlink -f .)"
+        export MOAR_FOLDER
+    fi
 
+}
+nqp_folder () {
+    if [ ! "$NQP_FOLDER" ]; then NQP_FOLDER="../nqp"; fi
 }
 stage_1 () {
     check_prefix
-    NQP_FOLDER="../nqp"
+    nqp_folder
     ./Configure.pl --compiler=clang --coverage --optimize=0 --debug=3 --prefix="$MOAR_PREFIX" || exit 1
     make install || exit
     cd "$NQP_FOLDER" || exit 1
@@ -26,13 +32,14 @@ stage_1 () {
 }
 stage_2 () {
     check_prefix
-    cd ../nqp || exit 1
+    nqp_folder
+    cd "$NQP_FOLDER" || exit 1
     if [ ! -f nqp-profile ]; then echo "can't find nqp-profile"; exit 1; fi;
     if [ -e coverage ]; then mv -v coverage "coverage-$RANDOM" || exit 1; fi
     # RUNNING TESTS HERE
     DATE_VERSION_HEADER="$(printf "%s\n%s" "$(date --utc)" "$(./nqp --version 2> /dev/null)" )"
     printf "Starting tests\n%s\n" "$DATE_VERSION_HEADER"
-    mkdir -p html || exit 1
+    mkdir -p "$HTML" || exit 1
     PROVE_CMD="$(make -n test | tail -n 1 | sed 's/nqp-m/nqp-profile/')"
     echo PROVE_CMD $PROVE_CMD
     eval "$PROVE_CMD"
@@ -55,20 +62,20 @@ stage_3 () {
     # or if under other distros there's extra files included
     filelist_moar_bin () { llvm-cov report -instr-profile $PROFDATA ./moar | tail -n +3 | head -n -3 | cut -d ' ' -f 1 | grep -v 3rdparty; }
     if [ ! -f "$PROFDATA" ]; then printf "Could not find $PROFDATA\n"; exit 1; fi
-    if [ -e "html" ]; then mv -v html "html-$RANDOM"; fi
-    llvm-cov show -format=html -instr-profile $PROFDATA ./moar $(filelist_moar_bin) -output-dir=html/moar
-    llvm-cov show -format=html -instr-profile $PROFDATA ./libmoar.* $(filelist_libmoar) -output-dir=html/libmoar
-    #mv html/index.html html/cov-index.html
+    if [ -e "$HTML" ]; then mv -v $HTML "$HTML-$RANDOM"; fi
+    llvm-cov show -format=html -instr-profile $PROFDATA ./moar $(filelist_moar_bin) -output-dir="$HTML/moar"
+    llvm-cov show -format=html -instr-profile $PROFDATA ./libmoar.* $(filelist_libmoar) -output-dir="$HTML/libmoar"
     # If env var isn't set then we must be running stage_3 by itself, so set it to 0
     # to not cause an error when we check it numerically
     if [ "$UNCLEAN_TEST" = "" ]; then UNCLEAN_TEST=0; fi
     if [ "$UNCLEAN_TEST" -gt 0 ]; then
         UNCLEAN_TEST_TXT="Unclean test, returned status $UNCLEAN_TEST"
     fi
+    # Old code not needed with LLVM 5, but maybe helpful on those on lower versions
     #if [ "$(which ansi2html)" ]; then
-    #    printf "$DATE_VERSION_HEADER\n$UNCLEAN_TEST_TXT\n\n%s\n" "$(faketty llvm-cov report -instr-profile $PROFDATA ./moar $(filelist_moar_bin) )" | ansi2html > html/report-moar.html && \
+    #    printf "$DATE_VERSION_HEADER\n$UNCLEAN_TEST_TXT\n\n%s\n" "$(faketty llvm-cov report -instr-profile $PROFDATA ./moar $(filelist_moar_bin) )" | ansi2html > "$HTML/report-moar.html" && \
     #        printf "Done creating html/report-moar.html\n"
-    #    printf "$DATE_VERSION_HEADER\n$UNCLEAN_TEST_TXT\n\n%s\n" "$(faketty llvm-cov report -instr-profile $PROFDATA ./libmoar.* $(filelist_libmoar) )" | ansi2html > html/report-libmoar.html && \
+    #    printf "$DATE_VERSION_HEADER\n$UNCLEAN_TEST_TXT\n\n%s\n" "$(faketty llvm-cov report -instr-profile $PROFDATA ./libmoar.* $(filelist_libmoar) )" | ansi2html > "$HTML/report-libmoar.html" && \
     #        printf "Done creating html/report-libmoar.html\n"
     #else
     #    printf "Could not find ansi2html, so not able to output function based reporting\n"
@@ -91,8 +98,5 @@ esac
 stage_1
 stage_2
 stage_3
-
-#echo $(filelist)
-#exit;
 
 # scp -r ./html/* root@cry.nu:/srv/cry.nu/coverage/
